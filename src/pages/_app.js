864f4e9cc2f2251b85228629733cd0d27af2720b
Stackbit Code Editor <projects@stackbit.com>
2021-12-01 11:52:14 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }) {
         const handleRouteChangeStart = async () => {
 
             const response = await fetch('/api/test');
-            alert();
+            alert(await response.json());
             if (window.onNextjsRouteChangeStart) {
                 window.onNextjsRouteChangeStart();
             }
